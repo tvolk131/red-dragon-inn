91cf9550008cc32782212ff973005e4d25218549
Tommy Volk <tvolk131@gmail.com>
2021-12-28 05:04:55 +0000

    Basic server definition

---
+++ server/src/main.rs
@@ -0,0 +1,14 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::content;
+
+#[get("/healthz")]
+async fn healthz_handler() -> content::Html<String> {
+    content::Html("<html><body><h1>200 OK</h1>Service ready.</body></html>".to_string())
+}
+
+#[rocket::launch]
+async fn rocket() -> _ {
+    rocket::build().mount("/", routes![healthz_handler])
+}
