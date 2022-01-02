fd2fd78aa4898c0bc9c739d1ef309d6be2fcf874
Tommy Volk <tvolk131@gmail.com>
2022-01-02 20:51:34 +0000

    Implement 'I raise!` card

--- server/src/game/player_card.rs
+++ server/src/game/player_card.rs
@@ -26,3 +26,15 @@ impl PlayerCard for GamblingImInPlayerCard {
         }
     }
 }
+
+struct IRaiseCard {}
+
+impl PlayerCard for IRaiseCard {
+    fn can_play(&self, player_uuid: &PlayerUUID, game: &GameLogic) -> bool {
+        game.gambling_round_in_progress() && game.is_gambling_turn(player_uuid) && !game.gambling_need_cheating_card_to_take_control()
+    }
+
+    fn play(&self, player_uuid: &PlayerUUID, game: &mut GameLogic) {
+        game.gambling_ante_up()
+    }
+}
